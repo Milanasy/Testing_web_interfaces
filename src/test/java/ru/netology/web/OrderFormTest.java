8c2287e2f9fe8e052734d34e6cf39cb06a27296d
Anastasiia Rakhmanova <rar.anastasiya@gmail.com>
2023-08-24 07:57:13 +0300

    initial commit

---
+++ src/test/java/ru/netology/web/OrderFormTest.java
@@ -0,0 +1,44 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class OrderFormTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static  void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestSomething() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        Thread. sleep (500);
+    }
+}
